7f3f410dee91739579e0131941c1a85b5a98ebf8
Lamine <feedzy>
2018-12-28 19:34:41 +0000

    utlisation des fichier css

--- app/controllers/default_controller.rb
+++ app/controllers/default_controller.rb
@@ -2,4 +2,9 @@ class DefaultController < ApplicationController
   def index
     @lamine = "Bonjour tous le monde "
   end
+
+  def feedzy
+    @page1 =" PAGE 2"
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'default/index'
-
+  root  'default#index'
+  get 'page1'=> 'default#feedzy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
